202020f36d1ae05c216f77863291abfb70ee4bf7
Leonardo <leonardorive6@gmail.com>
2022-11-28 15:14:28 -0800

    Merge with ui/ux and products

--- doca-store-project/src/main/java/com/onlinepetstore/docastoreproject/controller/HomeController.java
+++ doca-store-project/src/main/java/com/onlinepetstore/docastoreproject/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.onlinepetstore.docastoreproject.controller;
 
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.onlinepetstore.docastoreproject.domain.model.Product;
+import com.onlinepetstore.docastoreproject.service.ProductService;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping
-    public String showData() {
-        return "home";
+    public String home() {
+        return "store/home";
+    }
+
+    @GetMapping
+    @RequestMapping("/product")
+    public String product(Model model) {
+        List<Product> productList = productService.selectAll();
+        model.addAttribute("productList", productList);
+        return "store/product";
     }
 
 }
